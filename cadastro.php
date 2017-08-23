b2a6f900bc8a560f39dcfda302d6e748ed7e4123
IsaRallon <isadorarallon@gmail.com>
2017-08-23 20:09:37 -0300

    login quase pronto

--- cadastro.php
+++ cadastro.php
@@ -1,34 +1,24 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Duchy</title>
-</head>
-
-<body>
-
 <?php
-	echo "Funciona?";
+
 // RECEBENDO OS DADOS PREENCHIDOS DO FORMULÁRIO !
 	$nome= $_POST ["nomec"];//atribuição do campo "nome" vindo do formulário para variavel
 	$email= $_POST ["emailc"];//atribuição do campo "email" vindo do formulário para variavel
 	$senha= $_POST ["senhac"];//atribuição do campo "senha" vindo do formulário para variavel
  	$login= $_POST ["login"];
- 	echo "<br>", $nome, "<br>", $email, "<br>", $senha,  "<br>", $login;
 //Gravando no banco de dados ! conectando com o localhost - mysql
+ 	
  	$conexao = mysqli_connect("localhost","root","root","duchy"); 
-	if (!$conexao) {
-		die ("Erro de conexão com localhost, o seguinte erro ocorreu -> ".mysqli_connect_error()); //TODO: direcionar p página de erro
-	} else {
-
+	
+	if (!$conexao)
+		die ("Erro de conexão com localhost, o seguinte erro ocorreu -> ".mysqli_connect_error()); //TODO: direcionar p página de erro //
+		//conectando com a tabela do banco de dados
 		$query = "SELECT us_email from usuario where us_email=\"$email\"";
-		echo "<br>", $query;
 	    if ($result = mysqli_query($conexao,$query)) {
 	        $row = $result->fetch_row();
 	        if ($row[0] == null || $row[0] == "") {
 	            //Query que realiza a inserção dos dados no banco de dados na tabela indicada acima
-	    	    $query = "INSERT INTO usuario (us_email, us_senha, us_nome, us_user) VALUES (\"$email\", \"$senha\", \"$nome\", \"$login\")";
-	    	    echo "<br>", $query;
+	    	    $query = "INSERT INTO usuario (us_email, us_senha, us_nome, us_user, us_admin) VALUES (\"$email\", \"$senha\", \"$nome\", \"$login\", NULL)";
+
 	    	    if($result = mysqli_query($conexao,$query))
 	    	        echo "Seu cadastro foi realizado com sucesso!"; //TODO: direcionar p pag de Cadastro realizado com sucesso
 	            else
@@ -44,6 +34,3 @@
 
  	}
 ?>
-
-</body>
-</html>
---
+++ login.php
@@ -0,0 +1,40 @@
+<?php
+   include("teste.php");
+   session_start();
+
+ 	$user_check = $_SESSION['login_user'];
+   
+   $ses_sql = mysqli_query($db,"select email from admin where email = '$user_check' ");
+   
+   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+   
+   $login_session = $row['email'];
+   
+   if(!isset($_SESSION['login_user'])){
+      header("location:logado.html");
+   }
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $email = mysqli_real_escape_string($db,$_POST['email']);
+      $senha = mysqli_real_escape_string($db,$_POST['senha']); 
+      
+      $sql = "SELECT id FROM admin WHERE username = '$email' and passcode = '$senha'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("email");
+         $_SESSION['login_user'] = $email;
+         
+      }else {
+         $error = "O seu login está inválido";
+      }
+   }
+?>
